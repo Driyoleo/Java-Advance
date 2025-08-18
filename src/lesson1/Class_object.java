c03cd12b69e82701f3480bd8559a4b3b21475e7a
driyo <debashisr433@gmail.com>
2025-08-18 16:23:37 +0530

    init

---
+++ src/lesson1/Class_object.java
@@ -0,0 +1,38 @@
+package lesson1;
+
+// lesson 1: class and object
+
+import java.util.Scanner;
+
+// class
+class Car{
+
+    // instance variables
+    String company ;
+    String model ;
+
+    // instance method
+    public void getDetails(){
+        System.out.println(model + " car by " + company);
+    }
+}
+
+
+public class Class_object{
+    public static void main(String[] a){
+        Scanner input = new Scanner(System.in) ;
+
+        // object initialization
+        Car c1 = new Car() ;
+
+        // input
+        System.out.println("company name : ");
+        c1.company = input.nextLine() ;
+        System.out.println("model name : ");
+        c1.model = input.nextLine() ;
+
+        // output
+        c1.getDetails();
+
+    }
+}
---
+++ src/lesson2/Instance_static.java
@@ -0,0 +1,43 @@
+package lesson2 ;
+
+// lesson 2: Instance and Static variable and methods
+
+
+// class
+class Product{
+
+    static String name ;
+    String color ;
+
+    public void show(){
+        System.out.println(name + " : " + color);
+    }
+
+    /*
+        // wrong static method definition
+    public static void show(){
+        System.out.println(name + " : " + color);
+    }
+     */
+}
+
+
+public class Instance_static {
+    public static void main(String[] a){
+
+        // initialization of static variable
+        Product.name = "pen" ;
+
+        // initialization of instance variable among objects
+        Product pen1 = new Product() ;
+        pen1.color = "black" ;
+
+        Product pen2 = new Product() ;
+        pen2.color = "blue" ;
+
+        // output
+        pen1.show();
+        pen2.show();
+
+    }
+}
